f4c36a0831227a51fb40a8cfb97f479fed3ae536
Sanjidul Hoque <sanjid133@gmail.com>
2022-08-25 00:21:26 +0600

    Initial public page

---
+++ _next/static/x9_PidfO6vCJkPsolME_8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-86e80a395b2c2e6e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
